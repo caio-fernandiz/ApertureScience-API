24ea13719843f468316f6db72572127f5605f447
caio-fernandiz <arostuche@gmail.com>
2025-04-14 16:53:43 -0300

    criando metodo findbylogincode

--- src/main/java/br/com/aperturescience/infra/UserRepository.java
+++ src/main/java/br/com/aperturescience/infra/UserRepository.java
@@ -1,9 +1,10 @@
 package br.com.aperturescience.infra;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import br.com.aperturescience.models.Employee;
 
 public interface UserRepository extends JpaRepository<Employee, Long> {
-
+ UserDetails findByLoginCode(String loginCode);
 }
