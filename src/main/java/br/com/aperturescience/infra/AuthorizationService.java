1d0dbbd8787c1944db01a812ce02b0828d4e9c12
caio-fernandiz <arostuche@gmail.com>
2025-04-14 18:35:13 -0300

    Criando securitconfig

--- src/main/java/br/com/aperturescience/infra/AuthorizationService.java
+++ src/main/java/br/com/aperturescience/infra/AuthorizationService.java
@@ -1,5 +1,6 @@
 package br.com.aperturescience.infra;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,9 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthorizationService implements UserDetailsService{
 
+     @Autowired
+     UserRepository repository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return repository.findByLoginCode(username);
     }
 
 }
---
+++ src/main/java/br/com/aperturescience/infra/security/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package br.com.aperturescience.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    
+}
