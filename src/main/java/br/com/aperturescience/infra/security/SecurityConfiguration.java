b03c73e4b4b83eaa35d901b4489b62b057750818
caio-fernadiz <arostuche@gmail.com>
2025-04-15 10:59:17 -0300

    criando register de vdd agora

--- src/main/java/br/com/aperturescience/infra/security/SecurityConfiguration.java
+++ src/main/java/br/com/aperturescience/infra/security/SecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Configuration
 @EnableWebSecurity
@@ -37,4 +38,7 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @PostMapping("/register")
+    
 }
