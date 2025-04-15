eb326b9e418fa71982d208adf5e3613fd7f4a12e
caio-fernadiz <arostuche@gmail.com>
2025-04-15 10:58:39 -0300

    criando register

--- src/main/java/br/com/aperturescience/infra/AuthenticationController.java
+++ src/main/java/br/com/aperturescience/infra/AuthenticationController.java
@@ -1,17 +1,29 @@
 package br.com.aperturescience.infra;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("as/auth")
 public class AuthenticationController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+        
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.loginCode(), data.psswrd());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
 
+        return ResponseEntity.ok().build();
     }
 }
