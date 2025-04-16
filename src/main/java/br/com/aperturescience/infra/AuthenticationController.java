681ac7a67d760dfae60317ef3e7ce493f727f56d
caio-fernandiz <arostuche@gmail.com>
2025-04-16 15:56:06 -0300

    Merge branch 'sistemaLoginNote' into sistemaLoginPC

--- src/main/java/br/com/aperturescience/infra/AuthenticationController.java
+++ src/main/java/br/com/aperturescience/infra/AuthenticationController.java
@@ -4,16 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/as/auth")
-=======
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,30 +15,22 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("as/auth")
->>>>>>> 4e0a8f8594fab7c2a714a323580e8f47844ac4a8
 public class AuthenticationController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
-<<<<<<< HEAD
-    @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
-=======
     @Autowired
     private UserRepository repository;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
         
->>>>>>> 4e0a8f8594fab7c2a714a323580e8f47844ac4a8
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.loginCode(), data.psswrd());
         var auth = this.authenticationManager.authenticate(usernamePassword);
 
         return ResponseEntity.ok().build();
     }
-<<<<<<< HEAD
-=======
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
@@ -60,5 +42,4 @@ public class AuthenticationController {
         this.repository.save(newEmployee);
         return ResponseEntity.ok().build();
     }
->>>>>>> 4e0a8f8594fab7c2a714a323580e8f47844ac4a8
 }
--- src/main/java/br/com/aperturescience/infra/security/SecurityConfiguration.java
+++ src/main/java/br/com/aperturescience/infra/security/SecurityConfiguration.java
@@ -1,40 +1,20 @@
 package br.com.aperturescience.infra.security;
 
-<<<<<<< HEAD
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
-=======
->>>>>>> 4e0a8f8594fab7c2a714a323580e8f47844ac4a8
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-<<<<<<< HEAD
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-=======
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
->>>>>>> 4e0a8f8594fab7c2a714a323580e8f47844ac4a8
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-<<<<<<< HEAD
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/as/guineapigs").hasRole("DIRETOR")
-=======
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
@@ -45,15 +25,10 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.POST, "/as/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/as/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/as/tests").hasRole("DIRETOR")
->>>>>>> 4e0a8f8594fab7c2a714a323580e8f47844ac4a8
                         .anyRequest().authenticated()
                         )
                 .build();
     }
-<<<<<<< HEAD
-
-    public AuthenticationManager authenticationConfiguration
-=======
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();
@@ -65,5 +40,4 @@ public class SecurityConfiguration {
     }
 
  
->>>>>>> 4e0a8f8594fab7c2a714a323580e8f47844ac4a8
 }
