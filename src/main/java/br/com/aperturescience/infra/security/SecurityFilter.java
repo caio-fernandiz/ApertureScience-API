229824cf2ff96fd0470bc8499567651abfcccce8
caio-fernandiz <arostuche@gmail.com>
2025-04-16 18:12:36 -0300

    criando dofilterinternal e recovertoken no securityfilter

--- src/main/java/br/com/aperturescience/infra/security/SecurityFilter.java
+++ src/main/java/br/com/aperturescience/infra/security/SecurityFilter.java
@@ -1,5 +1,25 @@
  package br.com.aperturescience.infra.security;
 
-public class SecurityFilter {
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        var token = this.recoverToken(request);
+    }
+
+    private 
 
 }
