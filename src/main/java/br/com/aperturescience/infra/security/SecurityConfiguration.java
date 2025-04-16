3d94c2385c581c8c3e1f85e71f2bf51d92068c54
caio-fernandiz <arostuche@gmail.com>
2025-04-16 18:33:29 -0300

    importando securityfilter e finalizando filterbefoe no securityconfig

--- src/main/java/br/com/aperturescience/infra/security/SecurityConfiguration.java
+++ src/main/java/br/com/aperturescience/infra/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.aperturescience.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,6 +18,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfiguration {
     
+    @Autowired
+    SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
@@ -28,7 +32,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.POST, "/as/tests").hasRole("DIRETOR")
                         .anyRequest().authenticated()
                         )
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter)
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
     @Bean
--- src/main/java/br/com/aperturescience/infra/security/SecurityFilter.java
+++ src/main/java/br/com/aperturescience/infra/security/SecurityFilter.java
@@ -1,10 +1,14 @@
  package br.com.aperturescience.infra.security;
 
 import java.io.IOException;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import br.com.aperturescience.infra.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,13 +17,32 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         
         var token = this.recoverToken(request);
+        if(token !=null){
+            var login = tokenService.validateToken(token);
+            UserDetails employee = userRepository.findByLoginCode(login);
+
+            var authentication = new UsernamePasswordAuthenticationToken(employee, null, employee.getAuthorities()); 
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
     }
 
-    private 
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        
+        return authHeader.replace("Bearer ", "");
+    }
 
 }
